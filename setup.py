30e6928820e4cd1e72e0787b819177bba2bce126
nandurajendran <nandurajendran@yahoo.in>
2023-03-03 18:21:26 +0530

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='ML project using sensor',
+    author='Nandu',
+    license='MIT',
+)
